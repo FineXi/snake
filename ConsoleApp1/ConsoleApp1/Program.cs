006060d0eba9a39641dd12ee75476b894575f9ea
FineXi <ospkost@gmail.com>
2017-10-18 23:49:46 +0200

    first

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char sym1 = '*';
+            for (int x1 = 1; x1 <= 20; x1++)
+            {
+                for (int y1 = 1; y1 <= 20; y1++)
+                {
+                    if (x1 == y1)
+                    {
+                        Draw(x1, y1, sym1);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+            
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
